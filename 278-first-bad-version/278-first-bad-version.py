dbad6fbf998fa9d10134fed6e25fd43a89f4a199
Ahmed Amr <ahmedamron@gmail.com>
2022-05-17 06:57:57 +0300

    Time: 73 ms (5.09%), Space: 13.9 MB (13.03%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        while (left<=right):
+            mid = (left+right)//2
+            mid_is_bad = isBadVersion(mid)
+            if mid_is_bad:
+                right = mid-1
+            else:
+                left = mid+1
+        return left
+                
+            
+        

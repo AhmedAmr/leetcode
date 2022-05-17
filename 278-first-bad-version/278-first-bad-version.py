3d07d22b54e29d077ebacde44acb26895477135f
Ahmed Amr <ahmedamron@gmail.com>
2022-05-17 07:28:43 +0300

    Time: 56 ms (13.89%), Space: 13.8 MB (97.23%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -1,15 +1,29 @@
+import typing
+from bisect import bisect_left
 class Solution:
+    
+    ## PYTHIONIC SOLUTION PART 1
+    def __getitem__(self, index: int) -> bool:
+        return isBadVersion(index)
+    
+    
     def firstBadVersion(self, n: int) -> int:
-        left = 1
-        right = n
-        while (left<right):
-            mid = (left+right)//2
-            mid_is_bad = isBadVersion(mid)
-            if mid_is_bad:
-                right = mid
-            else:
-                left = mid+1
-        return left
+        ## FULL IMPL SOLUTION
+        # left = 1
+        # right = n
+        # while (left<right):
+        #     mid = (left+right)//2
+        #     mid_is_bad = isBadVersion(mid)
+        #     if mid_is_bad:
+        #         right = mid
+        #     else:
+        #         left = mid+1
+        # return left
+        
+        ## PYTHIONIC SOLUTION PART 2
+
+        return bisect_left(self, True, 1, n)
+        
                 
             
         

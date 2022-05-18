e7d023d3bc405165ed3d24beb756183ef515bf18
Ahmed Amr <ahmedamron@gmail.com>
2022-05-18 05:07:56 +0300

    Time: 65 ms (43.43%), Space: 15.2 MB (14.66%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,32 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        # this should handle multiple spaces in between words
-        def reverse_string(string: str) -> None:
-            s = list(string)
-            n = len(s)
-            i = 0
-            j = n-1
-            while(i<j):
-                s[i],s[j] = s[j],s[i]
-                i+=1
-                j-=1
-            return ''.join(s)
-        
-        n = len(s)
-        current_token = ''
-        output = ''
-        for i, char in enumerate(s):
-            if char != ' ':
-                current_token+=char
-            elif char == ' ' and current_token == '':
-                output+=' '
-            else:
-                reversed_token = reverse_string(current_token)
-                output+=reversed_token
-                output+=' '
-                current_token=''
-        if current_token:
-            reversed_token = reverse_string(current_token)
-            output+=reversed_token
-        return output
+        words = s.split(' ')
+        words_as_chars_list = list(map(list, words))
+        reversed_words_as_chars_list = list(map(reversed, words_as_chars_list))
+        reversed_words_list = list(map(''.join, reversed_words_as_chars_list))
+        return ' '.join(reversed_words_list)
         

30084e5f3abfde4ade13a3c896c1345cdfd9fb7e
Ahmed Amr <ahmedamron@gmail.com>
2022-05-20 15:54:52 +0300

    Time: 172 ms (37.95%), Space: 14 MB (68.27%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,6 +1,10 @@
 from collections import Counter
+from collections import defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        def drop_empty_values(map_: dict):
+            return defaultdict(int, {k:v for k,v in map_.items() if v!=0})
+    
         n = len(s1)
         m = len(s2)
         
@@ -8,11 +12,19 @@ class Solution:
             return False
         
         s1map = dict(Counter(s1))
+        s2map = defaultdict(int, dict(Counter(s2[0:n])))
         
-        for i in range(m-n+1):
-            s2map = dict(Counter(s2[i:i+n]))
+        if s1map == s2map:
+            return True
+        s2map = drop_empty_values(s2map)
+        
+        for i in range(1, m-n+1):
+            s2map[s2[i-1]]-=1
+            s2map[s2[i+n-1]]+=1
+            s2map = drop_empty_values(s2map)
             if s1map == s2map:
                 return True
         return False
-            
+    
+    
             

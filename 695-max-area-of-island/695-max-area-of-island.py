4fa7d5fd768bc3b35f42fcc187d8fadfd282a2c8
Ahmed Amr <ahmedamron@gmail.com>
2022-05-20 20:40:56 +0300

    Time: 233 ms (26.28%), Space: 17.8 MB (22.83%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -2,28 +2,12 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        
-        visited = [[False for _ in range(m)] for _ in range(n)]
-        max_ = 0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] != 0 and not visited[i][j]:
-                    max_ = max(max_, Solution.floodfill(grid,i,j,n,m,visited,0))
-        return max_
-    
-    @staticmethod
-    def floodfill(grid, r,c,n,m, visited, fill_count):
-        def is_valid(r,c):
-            return 0<=r<n and 0<=c<m and not visited[r][c] and grid[r][c] != 0
-        options = [(0,1), (1,0), (-1,0) , (0,-1)]
-        
-        if is_valid(r,c):
-            # fill
-            fill_count+=1
-            visited[r][c] = True
-        else:
-            return fill_count
-        max_ = fill_count
-        for option in options:
-            max_ = max(max_, Solution.floodfill(grid, r+option[0],c+option[1],n,m,visited,max_))
-        return max_
+        visited = set()
+        def area(r,c):
+            if not (0<= r < n and 0<= c < m and (r,c) not in visited and grid[r][c]):
+                return 0
+            
+            visited.add((r,c))
+            
+            return (1 + area(r+1, c) +  area(r-1,c) + area(r, c+1) + area(r, c-1))
+        return max(area(r,c) for r in range(n) for c in range(m))

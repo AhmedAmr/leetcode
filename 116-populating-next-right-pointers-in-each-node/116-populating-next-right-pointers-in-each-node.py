696f1df98f6621b787c213ce8349829f827e330c
Ahmed Amr <ahmedamron@gmail.com>
2022-05-21 05:37:46 +0300

    Time: 67 ms (81.86%), Space: 15.6 MB (75.73%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -1,3 +1,4 @@
+from collections import deque
 """
 # Definition for a Node.
 class Node:
@@ -10,13 +11,23 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        def link(left_subtree, right_subtree):
-            if left_subtree is None and right_subtree is None:
-                return
-            left_subtree.next = right_subtree
-            link(left_subtree.left, left_subtree.right)
-            link(right_subtree.left, right_subtree.right)
-            link(left_subtree.right, right_subtree.left)
+        if not root or root.left is None:
+            return root
+        q = deque([root.left, root.right, None])
         
-        link(root.left if root else None, root.right if root else None)
+        while(q != deque([None])):
+            while(q):
+                left_node = q.popleft()
+                right_node = q.popleft()
+                left_node.next = right_node
+                if left_node.left:
+                    q.append(left_node.left)
+                if left_node.right:
+                    q.append(left_node.right)
+                if right_node is None:
+                    break
+                q.appendleft(right_node)
+            q.append(None)
         return root
+                
+        

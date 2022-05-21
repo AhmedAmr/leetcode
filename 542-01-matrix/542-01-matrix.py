771decc9fc76d47585a427b8e7f2a6985616acea
Ahmed Amr <ahmedamron@gmail.com>
2022-05-21 09:25:13 +0300

    Time: 1251 ms (15.34%), Space: 17.3 MB (44.00%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -2,33 +2,31 @@ import sys
 from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        n = len(mat)
-        m = len(mat[0])
-        dest = [[sys.maxsize for _ in range(m)] for _ in range(n)]
+       
+        rows = len(mat)
+        cols = len(mat[0])
         
-        visited = set()
+        dest = [[sys.maxsize for _ in range(cols)] for _ in range(rows)] 
+        q = deque()
         
-        def is_valid(r,c):
-            return (0<=r<n and 0<=c<m and (r,c) not in visited)
-            
-            
-        q= deque()
-        for i in range(n):
-            for j in range(m):
+        # put all zeros in queue, as they represents starting points for bfs
+        for i in range(rows):
+            for j in range(cols):
                 if mat[i][j] == 0:
                     dest[i][j] = 0
-                    q.append((i,j,0))
-        options = [(1,0), (-1,0), (0,1),(0,-1)]
+                    q.append((i,j))
+                    
+        directions = [(1,0), (-1,0), (0,1),(0,-1)]
+        
         while(q):
-            cr,cc,co = q.popleft()
-            if is_valid(cr, cc):
-                visited.add((cr,cc))
-                dest[cr][cc] = min(dest[cr][cc], co)
-                for option in options:
-                    nr= cr+option[0]
-                    nc = cc+option[1]
-                    q.append((nr,nc, co+1))
-
+            x,y = q.popleft()
+            for direction in directions:
+                new_x = x + direction[0]
+                new_y = y + direction[1]
+                
+                if new_x >= 0 and new_y >= 0 and new_x < rows and new_y < cols:
+                    if dest[x][y] +1 < dest[new_x][new_y]:
+                        dest[new_x][new_y] = dest[x][y]+1
+                        q.append((new_x,new_y))
         return dest
-            
             

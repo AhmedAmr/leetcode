b014b87a7a56149f5d2c179aa7065f6edd59e3c4
Ahmed Amr <ahmedamron@gmail.com>
2022-05-22 08:50:31 +0300

    Time: 48 ms (67.74%), Space: 14.2 MB (22.48%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,25 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
+        soln = []
         
-        visited = [False for _ in range(n)]
-        solns = []
         
-        def solve(n, k, chosen):
-            if k == 0:
-                return solns.append(chosen)
-
-            for i, num in enumerate(nums):
-                if not visited[i]:
-                    # visit
-                    visited[i] = True
-                    
-                    # rec
-                    solve(n,k-1, chosen + [num])
-                    
-                    #backtrack
-                    visited[i] = False
+        def heap_permute(l, size):
+            if size == 1:
+                soln.append(l.copy())
+            
+            for i in range(size):
+                heap_permute(l, size-1)
+                
+                if size%2==0:
+                    # even, swap ith with last
+                    l[i],l[size-1] = l[size-1],l[i]
+                else:
+                    # odd, swap first with last
+                    l[0], l[size-1] = l[size-1],l[0]
+        heap_permute(nums, len(nums))           
+        return soln
+            
         
-        solve(n,n, [])
-        
-        return solns

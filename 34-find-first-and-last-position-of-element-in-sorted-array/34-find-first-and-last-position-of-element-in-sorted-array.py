0adaad0eac481766ece9346b98741b0ab23e701c
Ahmed Amr <ahmedamron@gmail.com>
2022-05-25 15:44:43 +0300

    Time: 125 ms (36.33%), Space: 15.5 MB (50.75%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,16 +1,34 @@
 import bisect
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1,-1]
-        n = len(nums)
-        start = bisect.bisect_left(nums, target)
-        end =  bisect.bisect_right(nums, target) - 1
-        if start >= n or nums[start] != target:
-            start = -1
-        if end >= n or nums[end] != target:
-            end = -1
-        return [start,end]
+        def bin_search(nums, target, left, right, direction='left'):
+            if left > right:
+                return -1
+            n = len(nums)
+            while(left <= right):
+                mid = left+(right-left)//2
+                if nums[mid] == target:
+                    ## return the min between this finding the most left element 
+                    if direction == 'left':
+                        value = bin_search(nums, target, left, mid-1, direction=direction)
+                    else:
+                    # direction is right
+                        value = bin_search(nums, target, mid+1, right, direction=direction)
+                    if value != -1:
+                        return value
+                    return mid
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return -1
+        return [
+            bin_search(nums, target, 0, len(nums)-1),
+            bin_search(nums, target, 0, len(nums)-1, direction='right')
+        ]
+        
+        
+        
             
         
         

6a9746b2cfd3641bf6c85ac65adfd943ffd10c94
Ahmed Amr <ahmedamron@gmail.com>
2022-05-29 23:15:20 +0300

    Time: 299 ms (37.12%), Space: 14.3 MB (51.57%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,35 +1,25 @@
 class Solution:
-    class QuickUnionFindByRank():
-        def __init__(self, n: int):
+    class UnionQuickFind():
+        def __init__(self, n):
             self.n = n
             self.roots = list(range(self.n))
-            self.ranks = [0] * self.n
 
         def find(self, node: int) -> int:
-            if self.roots[node] == node:
-                return node
-            root = self.find(self.roots[node])
-            self.roots[node] = root
-            return root
+            return self.roots[node]
 
 
         def union(self, node1: int, node2: int) -> None:
             root1 = self.find(node1)
             root2 = self.find(node2)
-            rank1 = self.ranks[root1]
-            rank2 = self.ranks[root2]
 
-            if rank1 > rank2:
-                self.roots[root2] = root1
-            elif rank1 < rank2:
-                self.roots[root1] = root2
-            else:
-                self.roots[root2] = root1
-                self.ranks[root1] += 1
-
+            if root1 != root2:
+                for i, node in enumerate(self.roots):
+                    if node == root2:
+                        self.roots[i] = root1
 
         def is_connected(self, node1: int, node2: int) -> bool:
             return self.find(node1) == self.find(node2)
+
         
         def size(self):
             return len(set(self.roots))
@@ -37,15 +27,13 @@ class Solution:
 
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        uf = Solution.QuickUnionFindByRank(n)
+        uf = Solution.UnionQuickFind(n)
         
         for i in range(n):
             for j in range(n):
                 item = isConnected[i][j]
                 if item:
                     uf.union(i,j)
-        for i in range(n):
-            uf.find(i)
 
         return uf.size()
         
